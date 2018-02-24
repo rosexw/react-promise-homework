55170b41302fd1f931a5586540fd071a209d7bd7
rosexw <rose.wan@evermight.com>
2018-02-24 17:05:53 -0500

    Started the React Promise assignment, with the fetch inside component did mount

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { config } from './config';
 import './App.css';
 
+
+const APP_ID = config.APP_ID; // your app id here, provided to you when you sign up
+const APP_KEY = config.APP_KEY; // your app key here, provided to you when you sign up
+
+// this would be what I would make for the final URL
+const BASE_ENDPOINT = `https://api.edamam.com/search?app_id=${APP_ID}&app_key=${APP_KEY}`;
+// an example API request with a cake query would be
+// `${BASE_ENDPOINT}&q=cake`
+
 class App extends Component {
+  componentDidMount() {
+    fetch(`${BASE_ENDPOINT}&q=cake`)
+      .then(response => response.json())
+      .then((result) => console.log(result));
+  }
   render() {
     return (
       <div className="App">
